ccf1d79d8cb1ea0dfe7b631b2ce0af8973c7d198
Amal-Dahshan <amaldahshan11710@gmail.com>
2023-09-24 18:50:27 -0700

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -12,13 +12,14 @@
 
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j;
+	size_t i, j, min;
+	int temp;
 
-	for (i = 0 ; i < n - 1 ; i++)
+	for (i = 0 ; i < size - 1 ; i++)
 	{
 		min = i;
 
-		for (j = i + 1 ; j < n ; j++)
+		for (j = i + 1 ; j < size ; j++)
 		{
 			if (array[j] < array[min])
 				min = j;
